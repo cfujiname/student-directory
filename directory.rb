0706794551bd837d839b5bb66db11966ea76a5c1
cristina <c.fujiname@gmail.com>
2020-08-24 11:08:19 +0100

    Transform student names to Array, iterate over array

--- directory.rb
+++ directory.rb
@@ -1,19 +1,25 @@
 #3 changing the number of students into variable (refactoring)
 student_count = 11
 #1 putsing the list of students
+students = [
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
+]# students are put in an array to make it easier to use the code afterwards
+# iteration so no need to manually puts all the elements of array
 puts "The students of Villains Academy"
 puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
+students.each do |student|
+  puts student
+end
 #2 printing the total number of students
 puts "Overall, we have #{student_count} great students"
 #2 print() does NOT add a new line characters
