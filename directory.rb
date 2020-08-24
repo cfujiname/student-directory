208c278ca4505eb5db0353403b8ce7a0e8da47ad
cristina <c.fujiname@gmail.com>
2020-08-24 14:23:51 +0100

    Define methods to print the same thing

--- directory.rb
+++ directory.rb
@@ -1,5 +1,3 @@
-#3 changing the number of students into variable (refactoring)
-student_count = 11
 #1 putsing the list of students
 students = [
   "Dr. Hannibal Lecter",
@@ -13,14 +11,20 @@ students = [
   "The Joker",
   "Joffrey Baratheon",
   "Norman Bates"
-]# students are put in an array to make it easier to use the code afterwards
-# iteration so no need to manually puts all the elements of array
-puts "The students of Villains Academy"
-puts "-------------"
-students.each do |student|
-  puts student
+]
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
 end
-#2 printing the total number of students
-puts "Overall, we have #{student_count} great students"
-#2 print() does NOT add a new line characters
-#4 refactoring the last sentence with string interpolation, from print blabla print blabla to line above
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+print_header
+print(students)
+print_footer(students)
