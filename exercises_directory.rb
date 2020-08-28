1381272d749a73142cdf1daa910956ecf944ffd5
cristina <c.fujiname@gmail.com>
2020-08-28 06:11:37 +0100

    Print studetns with certain initial, print students with names with less than 12chars

--- exercises_directory.rb
+++ exercises_directory.rb
@@ -1,6 +1,6 @@
 def input_students
-  puts "Please enter the name of the students and their cohort."
-  puts "To finish, just hit return twice"
+  puts "Please enter the name of the students, their cohort, country of birth and hobby"
+  puts "To finish, just hit return 4 times"
 
   students_hash = {}
   name = gets.chomp
@@ -9,7 +9,7 @@ def input_students
   while !name.empty? do
     students_hash[name] = cohort
     puts "Now we have #{students_hash.size} students"
-    name = gets.chomp.to_s
+    name = gets.chomp
     cohort = gets.chomp
   end
   students_hash
@@ -30,13 +30,19 @@ def print_initial(students_hash)
   puts "Enter an initial to filter the students"
   letter = gets.chomp.to_s
   students_hash_initial_arr = students_hash.keys
-  puts students_hash_initial_arr.select {|name| name.start_with?(letter)}
+  puts "These students have their names beginning with #{letter}: #{students_hash_initial_arr.select {|name| name.start_with?(letter)}}"
 end
 
 def print_max12(students_hash)
   students_hash_initial_arr = students_hash.keys
-  puts students_hash_initial_arr.select {|name| name.length < 12}
+  puts "These students: #{students_hash_initial_arr.select {|name| name.length < 12}} have names shorter than 12 characters"
 end
+
+#def print_hobby_country(students_hash)
+#  students_hash.each do |key, value|
+#    puts "#{name}'s country of birth is #{country} and loves #{hobby}"
+#  end
+#end
 
 def print_footer(students_hash)
   puts "Overall, we have #{students_hash.size} great students"
@@ -45,5 +51,7 @@ end
 students_hash = input_students
 print_header
 print(students_hash)
+print_initial(students_hash)
 print_max12(students_hash)
+print_hobby_country(students_hash)
 print_footer(students_hash)
